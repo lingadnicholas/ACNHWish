bbbafa545d7adb124bb019686a3413a577f9e615
Nicholas Lingad <lingadnicholas@engineering.ucla.edu>
2020-12-22 12:53:46 -0800

    TODO: Create a new villagerm put it in respective villagercol

--- src/components/acnhTable.js
+++ src/components/acnhTable.js
@@ -2,27 +2,49 @@ import './acnhTable.css';
 import React, { useState } from 'react'; 
 import v from './villager.png';
 
+/*When there is a valid value from the dropdown list, we want to:
+fetch the JSON object with that ID from the ACNH API
+Get all the data (Personality, Name, Picture, Birthday, Species, Gender, Catchphrase)
+Create a new Villager, place it in the respective VillagerColumn*/
+export async function GetVillager(value) {
+    const url = 'https://acnhapi.com/v1/villagers/'
+    const response = await fetch(url + value);
+    const data = await response.json();
+    const villagerInfo = {
+      key: value,
+      name: data.name['name-USen'],
+      personality: data.personality,
+      image_url: data.image_uri,
+      birthday: data.['birthday-string'],
+      species: data.species,
+      gender: data.gender,
+      catch_phrase: data['catch-phrase']
+    }
+    console.log(villagerInfo)
+    //TODO: Create a new villager, place it in the respective villagercolumn
+  }
+
 function Villager(prop)
 {
     return(
         <div className='villager'>
             <div>
-                <strong>Cyrano</strong>
+                <strong>{prop.name}</strong>
             </div>
             <div>            
-                <img className='icon' alt='Cyrano icon' src ={v}></img>
+                <img className='icon' alt={prop.name + 'icon'} src ={prop.image_url}></img>
             </div>
             <div>
-                <em>Birthday:</em> March 9th
+                <em>Birthday: </em>{prop.birthday}
             </div>
             <div>
-                <em>Species:</em> Anteater
+                <em>Species: </em>{prop.species}
             </div>
             <div>
-                <em>Gender:</em> Male
+                <em>Gender: </em>{prop.gender}
             </div>
             <div>
-                <em>Catchphrase:</em> "ah-CHOO" 
+                <em>Catchphrase: </em>{'"' + prop.catch_phrase + '"'}
             </div>
         </div>
     )
@@ -35,9 +57,8 @@ function VillagerColumn(prop)
             <strong>{prop.personality}</strong>
         </div>
         <div className='list'>
-            <div><Villager /></div>
-            <div><Villager /></div>
-            <div><Villager /></div>
+            <Villager name='test' birthday='test' species='test' gender='test' catch_phrase='test' image_url={v}/>
+            <Villager name='test2' birthday='test2' species='test2' gender='test2' catch_phrase='test2' image_url={v}/>
         </div>
     </div>
     )
--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SelectSearch from 'react-select-search'
+import { GetVillager } from './acnhTable.js'
 
 /* We need to fill up the villagers array first. 
 Get the array from the API, then sort by name 
@@ -15,28 +16,7 @@ const compare = (a, b) => {
   }
 
   var options = [];
-const url = 'https://acnhapi.com/v1/villagers/'
-/*When there is a valid value from the dropdown list, we want to:
-fetch the JSON object with that ID from the ACNH API
-Get all the data (Personality, Name, Picture, Birthday, Species, Gender, Catchphrase)
-Create a new Villager, place it in the respective VillagerColumn*/
 
-async function onChange(value) {
-  console.log(url+value)
-  const response = await fetch(url + value);
-  const data = await response.json();
-  const villagerInfo = {
-    key: value,
-    name: data.name['name-USen'],
-    personality: data.personality,
-    image_url: data.image_url,
-    species: data.species,
-    gender: data.gender,
-    catch_phrase: data['catch-phrase']
-  }
-  console.log(villagerInfo)
-  //TODO: Create a new villager, place it in the respective villagercolumn
-}
 
 export default class GetVillagerNames extends React.Component {
     state = {
@@ -44,6 +24,7 @@ export default class GetVillagerNames extends React.Component {
     }
 
     async componentDidMount() {
+      const url = 'https://acnhapi.com/v1/villagers/'
       const response = await fetch(url);
       const data = await response.json();
       for (var key in data) {
@@ -61,7 +42,7 @@ export default class GetVillagerNames extends React.Component {
       }
       return (
       <div>
-      <SelectSearch options={options} search="true" placeholder="Select villager" onChange={onChange} />
+      <SelectSearch options={options} search="true" placeholder="Select villager" onChange={GetVillager} />
       </div>
       )
     };
